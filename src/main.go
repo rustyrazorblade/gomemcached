5e4b346f747d8e33c73240be01c4812deed11989
Jon Haddad <jon@jonhaddad.com>
2013-10-03 00:21:23 -0700

    removed a lot of debugging, fixed channel issue.

--- src/main.go
+++ src/main.go
@@ -17,17 +17,17 @@ type Request struct {
 	Action string
 	Key string
 	Value string
-	Response string
+	Response chan string
 }
 
-func NewRequest(command string) Request {
+func NewRequest(command string, response_channel chan string) Request {
 	// trim the string
 	command = strings.TrimSpace(command)
 	args := strings.Split(command, " ")
 	if len(args) == 2 {
 		args = append(args, "")
 	}
-	return Request{args[0], args[1], args[2], ""}
+	return Request{args[0], args[1], args[2], response_channel}
 }
 
 func main() {
@@ -73,62 +73,41 @@ func data_manager(dm_chan chan Request) {
 		req := <-dm_chan
 		if req.Action == "set" {
 			data[req.Key] = req.Value
-			req.Response = req.Key
+			req.Response <- "OK"
 		} else if req.Action == "get" {
 			fmt.Println("get request:", req.Key)
 			fmt.Println(req)
-			req.Response = data[req.Key]
+			req.Response <- data[req.Key]
 		} else if req.Action == "delete" {
 			delete(data, req.Key)
-			req.Response = "OK"
+			req.Response <- "OK"
 		} else {
-			req.Response = "UNKNOWN"
+			req.Response <- "UNKNOWN"
 		}
-		dm_chan <- req
-		fmt.Println(data)
-
 	}
 }
 
 func handle_connection(conn net.Conn, data_manager chan Request) {
 	// read input
 	var buf = make([]byte, 1024)
+	response_channel := make(chan string)
 	for {
 		size, err := conn.Read(buf)
-		fmt.Println("Got buffer")
 		if err != nil {
 			fmt.Println("problem reading buffer maybe exit?")
 			return
 		}
-		fmt.Println("printing size and buffer")
-		fmt.Println(size)
-		fmt.Println(buf)
-
 		// to string
 		command := string(buf[:size])
 		command = strings.ToLower(command)
 
-		fmt.Println(command)
-
-		// back to bytes
-		//s := []byte(command)
-		//fmt.Println(s)
-
-
-//		fmt.Println(len(subs))
-//		fmt.Println(subs)
-
-		req := NewRequest(command)
-//		fmt.Println(req)
-
+		req := NewRequest(command, response_channel)
 		data_manager <- req
-		response := <-data_manager
 
-		conn.Write([]byte(response.Response + "\n"))
+		response := <- response_channel
 
-		// perform action
+		conn.Write([]byte(response + "\n"))
 
-		// return result
 	}
 }
 
